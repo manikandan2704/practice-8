0824f27913fe744a827d52eb6982a552f72439c5
manikandan <manimani19508@gmail.com>
2022-08-30 12:58:40 +0530

    add new file

--- js/script .js
+++ js/script .js
@@ -32,24 +32,99 @@ var fruits={
     return avg;
   }
 }
-var a=fruits.avg();
+var a=fruits.avg()
 console.log(fruits.avg());
 
 
 
-mobile={
+ var mobile={
     type:"honor 9 lite",
     battery:"5000mph",
     ram:"4gb",
     amount:"14000",
     storage:"64gb",
+};
+console.log(mobile);
 
-    
-    avg:function(){
-      var avg=(this.type+this.battery+this.ram+this.amount+this.storage);
+
+
+
   
-      return avg;
-    }
+ var google={
+
+  founder:"sergeybrin", 
+  year:"1998",
+  networth:"1210billion",
+  ceo:"sundar pichai",
+  numberofemployees:"139995",
+};
+console.log(google);
+
+
+
+var microsoft={
+
+  founder:"Bill gates", 
+  year:"1975",
+  networth:"1999.39billion",
+  ceo:"sundar pichai",
+  products:"microsoft",
+  numberofemployees:"221000",
+};
+console.log(microsoft);
+
+
+
+
+var watch={
+	
+    brand:"rolex",
+    type:"handwatch",
+    price:"2000",
+    color:"black",
+	quality:"first",
+};
+console.log(watch);
+
+
+
+var Note={
+    brand:"classmate",
+    size:"one quere",
+    price:"30",
+    color:"White paper",
+	totalpages:"200",
 }
-  var a=mobile.avg();
-  console.log(mobile.avg());
+console.log(Note);
+
+
+
+var Tv={
+    brand:"micromax",
+    model:"E213",
+    prize:"30k",
+    size:"42ing",
+	color:"solidblue",
+}
+console.log(Tv);
+
+var labtop={
+    brand:"Lenovo",
+    model:"E41",
+    price:"61000",
+    color:"blackk",
+	length:"35.5cms"
+}
+console.log(labtop);
+
+
+
+var periyar={
+	Name:"E.Ramasamy",
+	year:"1879-1973",
+	party:"dravidar kazhakam",
+	occupation:"activist,politician,social reformer,",
+}	
+console.log(periyar);
+	
+	
