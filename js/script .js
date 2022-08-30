aa53fea5f0f4177ecfb1f111302f0d8ec4d1f223
manikandan <manimani19508@gmail.com>
2022-08-30 09:20:08 +0530

    add new file

--- js/script .js
+++ js/script .js
@@ -1,15 +1,55 @@
+var bike={
+    model:200,
+    fueltype:100,
+    fuelcapacity:13,
+    mileage:30,
+    vehiclecc:130,
+
+
+    avg:function(){
+        var avg=(this.model+this.fueltype+this.fuelcapacity+this.mileage+this.vehiclecc)/5;
+
+        return avg;
+    }
+}
+var a=bike.avg();
+console.log(bike.avg());
 
 
 
-var cars = ["Saab", "", "hh"];
-console.log(cars);
- 
+
+var fruits={
+  fruitname:"apple",
+  protein:"0.3g",
+  totalfat:"0.2g",
+  fiber:"5 grams",
 
 
-var f="";
-console.log(f);
+
+  avg:function(){
+    var avg=(this.fruitname+this.protein+this.totalfat+this.fiber);
+
+    return avg;
+  }
+}
+var a=fruits.avg();
+console.log(fruits.avg());
 
 
-var v=12;
-var k="gghg";
-console.log(v/=k);
+
+mobile={
+    type:"honor 9 lite",
+    battery:"5000mph",
+    ram:"4gb",
+    amount:"14000",
+    storage:"64gb",
+
+    
+    avg:function(){
+      var avg=(this.type+this.battery+this.ram+this.amount+this.storage);
+  
+      return avg;
+    }
+}
+  var a=mobile.avg();
+  console.log(mobile.avg());
